bda34c0bab0043dac8a964b02eaf6b851a3675f1
YChaeeun <se_same5315@naver.com>
2020-01-17 01:37:23 +0900

    feat-#1 [dataBinding] 3.모델 클래스 바인딩
    - variable name="item"
    - @{item.dataOne}
    - @{Integer.toString(item.dataTwo)}

--- app/src/main/java/com/nexters/aac/MainActivity.kt
+++ app/src/main/java/com/nexters/aac/MainActivity.kt
@@ -2,11 +2,17 @@ package com.nexters.aac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nexters.aac.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val item = ItemModel("chaeeun", 24)
+        binding.setVariable(BR.item, item)
+        binding.executePendingBindings()
     }
 }
