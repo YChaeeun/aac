55a8913a89ed42df5a557f7eeab6457d3d58ecbd
YChaeeun <se_same5315@naver.com>
2020-01-17 16:28:24 +0900

    feat-#2 [dataBinding] code reformat

--- app/src/main/java/com/nexters/aac/MainActivity.kt
+++ app/src/main/java/com/nexters/aac/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.nexters.aac
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.nexters.aac.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-    val intArray:Array<Int> = arrayOf(1,2)
-    val strArray:Array<String> = arrayOf("하나", "둘")
+    val intArray: Array<Int> = arrayOf(1, 2)
+    val strArray: Array<String> = arrayOf("하나", "둘")
 
     val strList = mutableListOf<String>()
 
@@ -28,27 +27,27 @@ class MainActivity : AppCompatActivity() {
         binding.item = ItemModel("채은", 24)
 
         // eventBinding
-        binding.btnOne.setOnClickListener{
-            Toast.makeText(this, "btnClick_one",Toast.LENGTH_SHORT).show()
+        binding.btnOne.setOnClickListener {
+            Toast.makeText(this, "btnClick_one", Toast.LENGTH_SHORT).show()
         }
 
     }
 
     // eventBinding
-    fun onClickBtn(view: View){
-        Toast.makeText(this, "btnClick_two",Toast.LENGTH_SHORT).show()
+    fun onClickBtn(view: View) {
+        Toast.makeText(this, "btnClick_two", Toast.LENGTH_SHORT).show()
     }
 
     // eventBinding - Listener
-    fun onClickListener1(){
-        Toast.makeText(this, "Listener1",Toast.LENGTH_SHORT).show()
+    fun onClickListener1() {
+        Toast.makeText(this, "Listener1", Toast.LENGTH_SHORT).show()
     }
 
-    fun onClickListener2(item : ItemModel){
-        Toast.makeText(this, "Listener2 "+item.name,Toast.LENGTH_SHORT).show()
+    fun onClickListener2(item: ItemModel) {
+        Toast.makeText(this, "Listener2 " + item.name, Toast.LENGTH_SHORT).show()
     }
 
-    fun onClickListener3(view:View, item:ItemModel){
-        Toast.makeText(this, "Listener3 "+item.age,Toast.LENGTH_SHORT).show()
+    fun onClickListener3(view: View, item: ItemModel) {
+        Toast.makeText(this, "Listener3 " + item.age, Toast.LENGTH_SHORT).show()
     }
 }
